2e28e00464e4835d46b6ea6d610248787d61067b
micuch11 <micuch1126@gmail.com>
2024-03-04 09:34:10 +0000

    Method Class Assignment Submission

---
+++ Method Class Assignment Submission/Method Class Assignment Submission/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Method_Class_Assignment_Submission
+{
+    public class MathOperations
+    {
+        //Created a void method
+        public void Numbers(int num1, int num2)
+        {
+            //Math operation on num1
+            int result = num1 * 5;
+
+            //Display num2 to the screen
+            Console.WriteLine("Second integer: " + num2);
+        }
+    }
+}
---
+++ Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Method_Class_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathOperations mathOp = new MathOperations();
+
+            //Math operation on num1
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number * 5 equal: " + num1 * 5);
+
+            //Call the method in the class, passing in two numbers
+            mathOp.Numbers(5, 15);
+
+            Console.ReadLine();
+        }
+    }
+}
